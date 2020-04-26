c071adf966b90dcccc3cf68491e5da22d4f33f0d
github username <github email address>
2020-04-26 11:34:19 -0400

    Neaten up service file and add notes where needed.

--- lib/bartender/api.rb
+++ lib/bartender/api.rb
@@ -1,12 +1,12 @@
 class Api
   def self.get_drinks(ingredient)
-    ## what is my endpoint
+    ## what is my endpoint?
     url = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=#{ingredient}"
-    ## how do I go there and get what I need?
+    ## how do I go there and get the data I need?
     response = Net::HTTP.get(URI(url))
-    ## how do I handle that json and turn it into meanringful data
+    ## how do I handle that response and parse it into meaningful json?
     drinks = JSON.parse(response)["drinks"]
-    ### how do I make drink OBJECTS from that data
+    ### how do I make drink OBJECTS from that data?
     drinks.each do |drink_details|
       name = drink_details["strDrink"]
       drink_id = drink_details["idDrink"]
@@ -15,17 +15,15 @@ class Api
   end
 
   def self.getDrinkDetails(drink) ## drink_object
-    ## what is my endpoint
+    ## what is my endpoint?
     url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=#{drink.drink_id}"
-    ## how do I go there and get what I need
+    ## how do I go there and get the data I need
     response = Net::HTTP.get(URI(url))
-
+    # how do I parse the response into meaningful json data?
     drink_details = JSON.parse(response)["drinks"].first
-    # how do I parse the response
-    ## what do I want to do from there
+    ## what do I want to do from there?
     ## add an attribute on my EXISTING drink obj
     drink.instructions = drink_details["strInstructions"]
-    binding.pry
   end
 
 end
